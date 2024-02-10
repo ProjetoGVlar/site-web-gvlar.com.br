f4006ca7e655941652cf8586fb6070db2ba4f3aa
Milena Leme <milenaleme4@hotmail.com>
2024-02-10 02:43:23 -0300

    projeto

--- front-end/app_nodejs/main.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-const express = require('express');
-const app = express();
-const port = porta_da_aplicação;
-app.listen(port)
-
-const baseDir = `${__dirname}/build/`
-app.use(express.static(`${baseDir}`))
-app.get('/', (req, res) => res.sendfile('index.html' , { root : baseDir } ))
